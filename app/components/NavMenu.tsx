71abb35d89a65ad3d2ab02b85a22a2eddb339ec4
shiva <shiva.ping@gmail.com>
2026-01-19 14:13:28 +0530

    fixd Defer offscreen images

--- app/components/NavMenu.tsx
+++ app/components/NavMenu.tsx
@@ -45,7 +45,6 @@ export default function NavMenu() {
             width={40}
             height={40}
             className={styles.logo}
-            priority
           />
           <span className={styles.logoText}>GuruForU</span>
         </Link>
